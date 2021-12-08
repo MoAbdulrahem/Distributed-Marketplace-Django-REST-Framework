809948fd6676e0350a5b52455670d72771dd6bec
MoAbdulRahem <1601227@eng.asu.edu.eg>
2021-12-08 19:06:08 +0200

    finished the checkout process API

--- cart/views.py
+++ cart/views.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework import status
 # from rest_framework.status import Http404
 from accounts.serializers import RegisterSerializer
+from accounts.models import User
 from products.models import Product
 
 # from rest_framework.response import Response
@@ -42,10 +43,14 @@ class CartItemViewSet(viewsets.ModelViewSet):
     items = cart_instance.cart_item.all() # get all items associated with that car, by using reverse relation
 
     # TESTING: print the items instide the cart.
+    total_cost = 0
+    owners = {} #keep track of what each owner balance would be after their items are sold
     for item in items: #each item is a CartItem instance, so it has the product and its amount but NOT its price
       product_instance = Product.objects.get(pk=item.product.id)
       print(item.product, " Quantity: ", item.quantity, " ID: ", item.id, " price: ", product_instance.price)
-      
+      total_cost += (product_instance.price*item.quantity)
+      owners[product_instance.seller]= product_instance.price*item.quantity
+      print("total_cost: ", total_cost)
     # product_set = Product.objects.all()
     # products = product_set.cart_product.all()
     # for product in products:
@@ -54,17 +59,25 @@ class CartItemViewSet(viewsets.ModelViewSet):
     #Getting the product price
     # products = 
 
-
-    total_cost = 0
-    for item in items:
-      # product = Product.objects.get(user=request.user)
-      total_cost += item.product.price
+    serializer=RegisterSerializer(request.user)
 
     if request.user.balance >= total_cost:
-      print("we're in the if condition")
+      return_dict = {'status':'Transaction Complete, Balance was deducted by: '+str(total_cost)}
       request.user.balance -= total_cost
       request.user.save()
-      serializer=RegisterSerializer(request.user)
+      print("balance was deducted by ", total_cost)
+      for key in owners:
+        owner = User.objects.get(email=key)
+        owner.balance += owners[key]
+        owner.save()
+        return_dict[owner.email] = 'Balance increased by: '+str(owners[key])
+        print (key, "balance increased by:" , owners[key])
+
+      return Response(return_dict)
+
+    else:
+      print("transaction failed, Balance is not enough.")
+      return Response({'status':'Transaction Failed, Balance is not sufficient.'})
 
     return Response(serializer.data)
 
